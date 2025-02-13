3d5b39a0845b97546470ce6220a094f5a7f2930e
himadree-chaudhury <himchy2001@gmail.com>
2025-02-14 00:18:15 +0600

    firebase configure

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMvJXIRw_auGufvD31w7YVE1nGJmfR0PE",
+  authDomain: "ecoquest-himadree.firebaseapp.com",
+  projectId: "ecoquest-himadree",
+  storageBucket: "ecoquest-himadree.firebasestorage.app",
+  messagingSenderId: "661979470559",
+  appId: "1:661979470559:web:e9536bcfee2ca37bb0a76a",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
